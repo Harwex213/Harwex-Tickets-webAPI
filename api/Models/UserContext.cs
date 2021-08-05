cd5a4b4f1ba2d3e01944cd1ec23542784efbdcea
Harwex213 <harwex213@mail.ru>
2021-08-05 11:01:58 +0300

    initial commit

---
+++ api/Models/UserContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace api.Models
+{
+    public class UserContext : DbContext
+    {
+        public UserContext()
+        {
+        }
+
+        public UserContext(DbContextOptions<UserContext> options) : base(options)
+        {
+            // Database.EnsureCreated();
+        }
+
+        public DbSet<User> Users { get; set; }
+    }
+}
