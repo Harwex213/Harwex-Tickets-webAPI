d2f55836a4c7d73ea28ac68c58a6f0f230071b9f
Harwex213 <harwex213@mail.ru>
2021-08-05 23:40:25 +0300

    add Swagger

--- api/Startup.cs
+++ api/Startup.cs
@@ -5,6 +5,7 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.OpenApi.Models;
 
 namespace api
 {
@@ -24,12 +25,22 @@ namespace api
 
             var connectionString = Configuration.GetConnectionString("DefaultConnection");
             services.AddDbContext<UserContext>(opt => opt.UseSqlServer(connectionString));
+
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo {Title = "HarwexTicketsAPI", Version = "v1"});
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            if (env.IsDevelopment()) app.UseDeveloperExceptionPage();
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+                app.UseSwagger();
+                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "HarwexTicketsApi v1"));
+            }
 
             app.UseHttpsRedirection();
 
