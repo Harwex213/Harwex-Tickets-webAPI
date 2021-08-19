7a42179dc85ad4608ca4eb0e37ab0a49fb99b2c3
Harwex213 <harwex213@mail.ru>
2021-08-19 14:23:05 +0300

    feat(city service)

---
+++ Domain/Interfaces/Services/ICitiesService.cs
@@ -0,0 +1,9 @@
+﻿using Domain.Entities;
+
+namespace Domain.Interfaces.Services
+{
+    public interface ICitiesService : ICrudService<City>
+    {
+        
+    }
+}
---
+++ Service/Services/CityService.cs
@@ -0,0 +1,14 @@
+﻿using Domain.Entities;
+using Domain.Interfaces;
+using Domain.Interfaces.Services;
+using Service.Services.Abstract;
+
+namespace Service.Services
+{
+    public class CityService: CrudService<City>, ICitiesService
+    {
+        public CityService(IUnitOfWork unitOfWork) : base(unitOfWork)
+        {
+        }
+    }
+}
