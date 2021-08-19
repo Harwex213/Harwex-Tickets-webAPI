60bcc8e42cbbb8ab569615c4b0ee83ffced8d974
Harwex213 <harwex213@mail.ru>
2021-08-19 19:08:45 +0300

    feat(cinema service)

---
+++ Service/Services/CinemaMoviesService.cs
@@ -0,0 +1,14 @@
+﻿using Domain.Entities;
+using Domain.Interfaces;
+using Domain.Interfaces.Services;
+using Service.Services.Abstract;
+
+namespace Service.Services
+{
+    public class CinemaMoviesService : CrudService<CinemaMovie>, ICinemaMoviesService
+    {
+        public CinemaMoviesService(IUnitOfWork unitOfWork) : base(unitOfWork)
+        {
+        }
+    }
+}
