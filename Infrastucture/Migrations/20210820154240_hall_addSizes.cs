b3afb4f1ad08b21c8f1545bd14b14cf60321221e
Harwex213 <harwex213@mail.ru>
2021-08-20 18:56:48 +0300

    feat(db hall): add sizes

---
+++ Infrastucture/Migrations/20210820154240_hall_addSizes.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Infrastucture.Migrations
+{
+    public partial class hall_addSizes : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<short>(
+                name: "ColsAmount",
+                table: "Halls",
+                type: "smallint",
+                nullable: false,
+                defaultValue: (short)0);
+
+            migrationBuilder.AddColumn<short>(
+                name: "RowsAmount",
+                table: "Halls",
+                type: "smallint",
+                nullable: false,
+                defaultValue: (short)0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ColsAmount",
+                table: "Halls");
+
+            migrationBuilder.DropColumn(
+                name: "RowsAmount",
+                table: "Halls");
+        }
+    }
+}
